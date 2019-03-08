5a2c7f3e4bd94976ac454ec9d05622699cf7c531
Kenzie Baker <mbaker880@west-mec.org>
2019-03-08 16:41:49 -0700

    Changes to Hanjie Puzzles
    Caused the solutions to appear for each puzzle depending on which puzzle button was selected. Set the initial load up page to have all of the puzzles solutions to be covered by gold, but, if another puzzle is selected, the gold boxes wont cover up the solutions to the puzzles.

--- jpf_hanjie.js
+++ jpf_hanjie.js
@@ -56,11 +56,48 @@
 // Run the init function when the page loads
 window.onload = init;
 
+var puzzleCells;
+
 function init() {
       // Insert the title for the first puzzle
       document.getElementById("puzzleTitle").innerHTML = "Puzzle 1";
       // Insert the html code for the firt puzzle table
-      document.getElementById("puzzle").innerHTML = drawPuzzle(puzzleHint, puzzleRating, puzzle1);
+      document.getElementById("puzzle").innerHTML = drawPuzzle(puzzle1Hint, puzzle1Rating, puzzle1);
+      // Add event handlers for the puzzle buttons
+      var puzzleButtons = document.getElementsByClassName("puzzles");
+      for(var i = 0; i < puzzleButtons.length; i++) {
+            puzzleButtons[i].onclick = swapPuzzle;
+      }
+      setupPuzzle();
+}
+
+function swapPuzzle(e) {
+      // Retrieve the id of the clicked button
+      var puzzleID = e.target.id;
+      // Retrieve the value of the clicked button
+      var puzzleTitle = e.target.value;
+      document.getElementById("puzzleTitle").innerHTML = puzzleTitle;
+      // Display the puzzle based on the value of the puzzle id variable
+      switch(puzzleID) {
+            case "puzzle1":
+                  document.getElementById("puzzle").innerHTML = drawPuzzle(puzzle1Hint, puzzle1Rating, puzzle1);
+                  break;
+            case "puzzle2":
+                  document.getElementById("puzzle").innerHTML = drawPuzzle(puzzle2Hint, puzzle2Rating, puzzle2);
+                  break;
+            case "puzzle3":
+                  document.getElementById("puzzle").innerHTML = drawPuzzle(puzzle3Hint, puzzle3Rating, puzzle3);
+                  break;
+      }
+}
+
+function setupPuzzle() {
+      // Match all of the datta cells within the puzzle
+      puzzleCells = document.querySelectorAll("table#hanjieGrid td");
+      // Set the initial color of each cell to gold
+      for(var i = 0; i < puzzleCells.length; i++) {
+            puzzleCells[i].style.backgroundColor = "rgb(233, 207, 29)";
+      }
 }
 
 
